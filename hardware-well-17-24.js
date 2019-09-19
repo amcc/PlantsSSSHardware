a0944959882a39f980f3492a9883805975c25d35
amcc <alistairmcclymont@gmail.com>
2019-09-19 16:10:24 +0100

    Add files via upload

---
+++ hardware-well-17-24.js
@@ -0,0 +1,66 @@
+module.exports = [
+  {
+    id: 'plant17',
+    pump: 14,
+    sensor: 26,
+    plant: "ruderal",
+    description: "well back right upper",
+    side: 'right'
+  },
+  {
+    id: 'plant18',
+    pump: 15,
+    sensor: 19,
+    plant: "bee pollinators",
+    description: "well front right upper",
+    side: 'right'
+  },
+  {
+    id: 'plant19',
+    pump: 18,
+    sensor: 13,
+    plant: "sunflower",
+    description: "well front left upper",
+    side: 'left'
+  },
+  {
+    id: 'plant20',
+    pump: 23,
+    sensor: 6,
+    plant: "pteris cretica",
+    description: "well center right lower",
+    side: 'left'
+  },
+  {
+    id: 'plant21',
+    pump: 24,
+    sensor: 5,
+    plant: "draca",
+    description: "well middle right upper",
+    side: "right"
+  },
+  {
+    id: 'plant22',
+    pump: 25,
+    sensor: 0,
+    plant: "green cinkly leaves",
+    description: "well middle left lower",
+    side: "left"
+  },
+  {
+    id: 'plant23',
+    pump: 8,
+    sensor: 11,
+    plant: "three bee polinators",
+    description: "well left middle top",
+    side: "left"
+  },
+  {
+    id: 'plant24',
+    pump: 7,
+    sensor: 9,
+    plant: "bee polinators",
+    description: "well left centre top",
+    side: "left"
+  }
+];
